b94117615334c0e36910e492ee7d307fe89ddb70
Prince of persia Dastan <60606830+SatanD01@users.noreply.github.com>
2022-01-14 19:29:37 +0500

    module 2 lesson 1 Конфигурация webpack на клиентеÐ

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export function Header(){
+    return (
+        <header>
+            <h1>
+                Reddit for our own
+            </h1>
+        </header>
+    )
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const NODE_ENV = process.env.NODE_ENV;
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const IS_DEV = NODE_ENV == 'development';
+const IS_PROD = NODE_ENV == 'production';
+
+
+
+function setupDevtool() {
+    if (IS_DEV) return 'eval';
+    if (IS_PROD) return false;
+}
+module.exports = {
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', 'tsx', '.json']
+    },
+    mode: NODE_ENV ? NODE_ENV : 'development',
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    module: {
+        rules: [{
+            test: /\.[tj]sx?$/,
+            use: ['ts-loader']
+        }]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({template: path.resolve((__dirname, 'index.html'))})
+    ],
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: IS_DEV
+    },
+    devtool: setupDevtool()
+};
