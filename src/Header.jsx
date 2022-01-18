c4d851ba94dea2658f2579b639f19afe65ab0048
Prince of persia Dastan <60606830+SatanD01@users.noreply.github.com>
2022-01-18 13:48:26 +0500

    HW-2.6

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
-import style from './Header.css'
+import {HelloReact} from "./HelloReact";
 export function Header(){
     return (
         <header>
-            <h1 className={style.example}>
-                Reddit for our own
-            </h1>
+            <HelloReact />
         </header>
     )
 }
---
+++ src/HelloReact.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import style from './Header.css'
+export function HelloReact() {
+    return (
+        <h1 className={style.example}>
+            Hello React
+        </h1>
+    )
+}
