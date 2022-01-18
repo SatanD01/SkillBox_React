e65b048a8819197b1f72f3f534ba0d2f7d67805f
Prince of persia Dastan <60606830+SatanD01@users.noreply.github.com>
2022-01-18 13:05:40 +0500

    css-modules

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-
+import style from './Header.css'
 export function Header(){
     return (
         <header>
-            <h1>
+            <h1 className={style.example}>
                 Reddit for our own
             </h1>
         </header>
